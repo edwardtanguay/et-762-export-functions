e2516e38661d5d2e221a42df152d9f4fc6a9390e
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-03 14:06:16 +0200

    finished

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import * as tools from './tools';
+
+function App() {
+
+
+  return (
+    <>
+    <h1>Export function test</h1>
+      <p>Test111: {tools.test111()}</p> 
+    </>
+  )
+}
+
+export default App
---
+++ src/tools.ts
@@ -0,0 +1,7 @@
+export const test111 = () => {
+	return 'test111';
+}
+
+export const test222 = () => {
+	return 'test222';
+}
